01dc02d1f992a8c7225f9fdec9e1c67d283d9259
Rinze <rinze.bruining@tno.nl>
2023-09-21 11:45:54 +0200

    Published branch "main"

---
+++ dist/components/field.component.d.ts
@@ -0,0 +1,16 @@
+import { Echoable } from '../interfaces/echoable';
+import { BaseComponent } from './base.component';
+export declare class FieldComponent extends BaseComponent implements Echoable {
+    name: string;
+    nonNullableAssertion: boolean;
+    preserveDefaultNullable: boolean;
+    nullable: boolean;
+    useUndefinedDefault: boolean;
+    default?: string;
+    type?: string;
+    echo: () => string;
+    constructor(obj: {
+        name: string;
+        useUndefinedDefault: boolean;
+    });
+}
