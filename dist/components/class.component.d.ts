e816b336269b536932c0e18cb6dd2b9ecfe4a2c2
Rinze <rinze.bruining@tno.nl>
2023-09-21 11:41:29 +0200

    Published branch "main"

---
+++ dist/components/class.component.d.ts
@@ -0,0 +1,13 @@
+import { Echoable } from '../interfaces/echoable';
+import { FieldComponent } from './field.component';
+import { BaseComponent } from './base.component';
+export declare class ClassComponent extends BaseComponent implements Echoable {
+    name: string;
+    fields?: FieldComponent[];
+    relationTypes?: string[];
+    enumTypes?: string[];
+    extra?: string;
+    types?: string[];
+    echo: () => string;
+    reExportPrefixed: (prefix: string) => string;
+}
